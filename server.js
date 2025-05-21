2e1fea54cabd9f50a36c1b061e2e68679b097711
Aafiyan <Co.2024.aspathan@bitwardha.ac.in>
2025-05-21 21:07:21 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join", ({ mode }) => {
+    console.log(`${socket.id} joined mode: ${mode}`);
+    // Room assignment logic can go here later
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Socket.IO server running on port ${PORT}`);
+});
